b5dbd861987a7adc5f08b68c8b20c3c62aaa23c8
SushmaRamakrishna <urpalshu@gmail.com>
2021-02-17 19:33:25 -0600

    Update index.js
    mutation added and resolvers added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import schema from "./schema";
 import  { graphqlHTTP } from 'express-graphql'
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,36 +9,8 @@ app.get('/', (req,res) => {
     res.send('Graphql is amazing!');
 });
 
-const FriendDatabase = {};
 
-class Friend {
-    constructor(id, { firstName, lastName, email, gender}) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.gender = gender
-    }
-}
-
-//const root = { hello: () => "hi! i am sushma"};
-const root = { 
-    friend: () => {
-    return {
-        "id": 3354534,
-        "firstName": "Sohum",
-        "lastName": "Gorladku",
-        "gender": "female",
-        "email":  "test1@gmail.com",               
-    }
-    },
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        FriendDatabase[id] = input;
-        return new Friend(id, input);
-    }
-
-};
+const root = resolvers;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
