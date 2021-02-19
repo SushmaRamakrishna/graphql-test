47e7f24fc7976a673606873219334d4414266b5e
SushmaRamakrishna <urpalshu@gmail.com>
2021-02-18 20:34:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-import express from 'express';
+import express from "express";
 import schema from "./schema";
 import  { graphqlHTTP } from 'express-graphql'
-import resolvers from './resolvers';
+import { schema } from './schema';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Graphql is amazing!');
-});
-
-
-const root = resolvers;
-
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+   schema: schema,
+   graphiql: true,
 }));
 
-app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
+app.listen( 8080, () => console.log("Running on server port localhost:8080/graphql"));
