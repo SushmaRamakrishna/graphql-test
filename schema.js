251e226b51c4c6ce60f47c3d3588ab47b1eb0d05
SushmaRamakrishna <urpalshu@gmail.com>
2021-02-18 20:35:24 -0600

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,51 +1,56 @@
-import { buildSchema } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+import {resolvers } from "./data/resolvers";
 
-const schema = buildSchema(`
+const typeDefs = `
       type Friend {
           id: ID,
           firstName: String,
           lastName: String,
           gender: Gender,
+          language: String,
           age: Int,
           email:String,
           contacts: [Contact]
       }
 
+      type Alien {
+          id: ID
+          firstName: String,
+          lastName: String,
+          planet: String
+      }
       type Contact {
           firstName: String,
           lastName: String
       }
-
       enum Gender {
           MALE
           FEMALE
           OTHER
       }
-
       type Query {
           getFriend(id: ID): Friend
       }
-
       input FriendInput {
         id: ID,
         firstName: String,
         lastName: String,
         gender: Gender,
+        language: String,
         age: Int,
         email:String,
         contacts: [ContactInput]
       }
-
       input ContactInput {
           firstName: String,
           lastName: String
       }
-
       type Mutation {
           createFriend(input: FriendInput): Friend
       }
-
       
-`)
+`;
 
-export default schema;
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+export {schema};
